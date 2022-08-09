1e53702f5874118f042bc64ad4cbb5300813867c
Juan Cassiano <juancassiano@hotmail.com>
2022-08-09 14:35:26 -0300

    Open and Close connection in main function.

--- src/main/java/TodoApp/App.java
+++ src/main/java/TodoApp/App.java
@@ -3,12 +3,15 @@
  */
 package TodoApp;
 
+import java.sql.Connection;
+import util.ConnectionFactory;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Connection c = ConnectionFactory.getConnection();
+        
+        ConnectionFactory.closeConnection(c);
     }
 }
