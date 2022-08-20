dc87493675fe8e6f17d8a3ada4e80da2d6cac2f9
Juan Cassiano <juancassiano@hotmail.com>
2022-08-20 20:50:30 -0300

    Create Close Statement Connection.

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -6,6 +6,7 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 /**
  *
@@ -31,6 +32,20 @@ public class ConnectionFactory {
         try{
             if(connection!= null){
                 connection.close();
+            }
+        }catch(Exception ex){
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        }
+    }
+    
+      public static void closeConnection(Connection connection, PreparedStatement statement){
+        try{
+            if(connection!= null){
+                connection.close();
+            }
+            
+            if(statement != null){
+                statement.close();
             }
         }catch(Exception ex){
             throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
