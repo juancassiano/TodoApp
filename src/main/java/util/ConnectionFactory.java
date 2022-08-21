a3dcb44373223fcfafb14b6c88b14dccf027ace0
Juan Cassiano <juancassiano@hotmail.com>
2022-08-20 22:37:19 -0300

    Refactory ConnectionFactory

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -7,6 +7,7 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  *
@@ -38,7 +39,7 @@ public class ConnectionFactory {
         }
     }
     
-      public static void closeConnection(Connection connection, PreparedStatement statement){
+    public static void closeConnection(Connection connection, PreparedStatement statement){
         try{
             if(connection!= null){
                 connection.close();
@@ -51,5 +52,23 @@ public class ConnectionFactory {
             throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
         }
     }
-    
+   
+    public static void closeConnection(Connection connection, PreparedStatement statement, ResultSet resultSet){
+        try{
+            if(connection!= null){
+                connection.close();
+            }
+            
+            if(statement != null){
+                statement.close();
+            }
+            if(resultSet != null){
+                resultSet.close();
+            }
+            
+        }catch(Exception ex){
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        }
+    }
+   
 }
