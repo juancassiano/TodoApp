a75e45890be95153dad048437801f9fcb37d6c79
Juan Cassiano <juancassiano@hotmail.com>
2022-08-20 21:39:43 -0300

    Create CRUD Methods for Project Controller.

---
+++ src/main/java/controller/PackageController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author juan
+ */
+public class PackageController {
+    
+    public void save(Project Project){}
+    
+    
+    public void update(Project Project){}
+    
+    
+    public void removeById(int idProject){}
+    
+    
+    public void List<Project> getAll(int idProject){}
+    
+    
+}
